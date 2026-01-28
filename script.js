a9bd33c2ff72eac34066fd7766658b5ef5957c91
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-01-28 21:12:57 +0100

    borrada informacion duplicada en variables

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-fetch('menu.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('menu-container').innerHTML = data;
+fetch("menu.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("menu-container").innerHTML = data;
   })
-  .catch(error => {
-    console.error('Error cargando el menú:', error);
+  .catch((error) => {
+    console.error("Error cargando el menú:", error);
   });
 
-  fetch('001-variables.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('content-container').innerHTML = data;
-      Prism.highlightAll();
-    })
-    .catch(error => {
-      console.error('Error cargando el contenido:', error);
-    });
+fetch("001-variables.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("content-container").innerHTML = data;
+    Prism.highlightAll();
+  })
+  .catch((error) => {
+    console.error("Error cargando el contenido:", error);
+  });
