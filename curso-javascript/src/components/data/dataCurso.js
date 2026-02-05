a13bfb361afe166e4cec01b94e2b2363124c59fc
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-05 20:25:49 +0100

    added slugs a paginas curso

--- curso-javascript/src/components/data/dataCurso.js
+++ curso-javascript/src/components/data/dataCurso.js
@@ -5,38 +5,44 @@ import htmlVariables4 from "../../004-tiposDatos.js";
 import htmlVariables5 from "../../005-ambitosVariables.js";
 import htmlVariables6 from "../../006-Operadores.js";
 
+
 export const dataCurso = {
-    paginas: [
-        {
-            id: 1,
-            titulo: "Variables en JavaScript",  
-            contenido: htmlVariables,
-        },
-        {
-            id: 2,
-            titulo: "Variables II en JavaScript",  
-            contenido: htmlVariables2,
-        }, 
-        {
-            id: 3,
-            titulo: "Variables III en JavaScript",  
-            contenido: htmlVariables3,
-        }, 
-        {
-            id: 4,
-            titulo: "Tipos de Datos en JavaScript",  
-            contenido: htmlVariables4,
-        }, 
-        {
-            id: 5,
-            titulo: "Ámbitos de variables en JavaScript",  
-            contenido: htmlVariables5
-        },
-        {
-            id: 6,
-            titulo: "Operadores en JavaScript",  
-            contenido: htmlVariables6
-        },
-        // Agregar mas paginas segun sea necesario
-    ]
-}
+  paginas: [
+    {
+      id: 1,
+      slug: "variables",
+      titulo: "Variables en JavaScript",
+      contenido: htmlVariables,
+    },
+    {
+      id: 2,
+      slug: "variables-ii",
+      titulo: "Variables II en JavaScript",
+      contenido: htmlVariables2,
+    },
+    {
+      id: 3,
+      slug: "variables-iii",
+      titulo: "Variables III en JavaScript",
+      contenido: htmlVariables3,
+    },
+    {
+      id: 4,
+      slug: "tipos-de-datos",
+      titulo: "Tipos de Datos en JavaScript",
+      contenido: htmlVariables4,
+    },
+    {
+      id: 5,
+      slug: "ambitos-de-variables",
+      titulo: "Ámbitos de variables en JavaScript",
+      contenido: htmlVariables5,
+    },
+    {
+      id: 6,
+      slug: "operadores",
+      titulo: "Operadores en JavaScript",
+      contenido: htmlVariables6,
+    },
+  ],
+};
--- curso-javascript/src/components/data/info.js
+++ curso-javascript/src/components/data/info.js
@@ -1,53 +1,57 @@
-export const infoCurso = [{
-  titulo: "Curso de JavaScript",
-  descripcion: "Variables en Javascript",
-  autor: "jmmroca",    
+// info.js
+export const infoCurso = [
+  {
+    slug: "variables",
+    titulo: "Curso de JavaScript",
+    descripcion: "Variables en Javascript",
+    autor: "jmmroca",
     fechaActualizacion: "2024-06-15",
     version: "1.0.0",
-    licencia: "MIT",    
-},
-{
-  titulo: "Curso de JavaScript",
-  descripcion: "Variables II en Javascript", 
+    licencia: "MIT",
+  },
+  {
+    slug: "variables-ii",
+    titulo: "Curso de JavaScript",
+    descripcion: "Variables II en Javascript",
     autor: "jmmroca",
     fechaActualizacion: "2024-05-20",
     version: "2.1.0",
     licencia: "GPL-3.0",
-},
-{
-  titulo: "Curso de JavaScript",
-  descripcion: "Variables III en Javascript",
-    autor: "jmmroca",  
+  },
+  {
+    slug: "variables-iii",
+    titulo: "Curso de JavaScript",
+    descripcion: "Variables III en Javascript",
+    autor: "jmmroca",
     fechaActualizacion: "2024-04-10",
     version: "3.0.2",
     licencia: "Apache-2.0",
-},
-
-{
-  titulo: "Curso de JavaScript",
-  descripcion: "Tipos de datos en Javascript",
+  },
+  {
+    slug: "tipos-de-datos",
+    titulo: "Curso de JavaScript",
+    descripcion: "Tipos de datos en Javascript",
     autor: "jmmroca",
     fechaActualizacion: "2024-03-15",
     version: "4.2.0",
     licencia: "MIT",
-},
-
-{
-  titulo: "Curso de JavaScript",
-  descripcion: "Ámbitos de variables en JavaScript",
+  },
+  {
+    slug: "ambitos-de-variables",
+    titulo: "Curso de JavaScript",
+    descripcion: "Ámbitos de variables en JavaScript",
     autor: "jmmroca",
     fechaActualizacion: "2024-02-20",
     version: "5.0.0",
     licencia: "Apache-2.0",
-},
-
-{
-  titulo: "Curso de JavaScript",
-  descripcion: "Operadores en JavaScript",
+  },
+  {
+    slug: "operadores",
+    titulo: "Curso de JavaScript",
+    descripcion: "Operadores en JavaScript",
     autor: "jmmroca",
     fechaActualizacion: "2024-02-20",
     version: "5.0.0",
     licencia: "Apache-2.0",
-},
-
+  },
 ];
