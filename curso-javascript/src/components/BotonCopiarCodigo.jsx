8e9cdd23046c51c6ffa45f68b88f027acb88a276
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-07 21:54:24 +0100

    fix BotonCopiarCodigo

--- curso-javascript/src/components/BotonCopiarCodigo.jsx
+++ curso-javascript/src/components/BotonCopiarCodigo.jsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import { Copy, Check } from "lucide-react";
 
 function BotonCopiarCodigo({ copySnippet }) {
+  const [copied, setCopied] = useState(false);
 
-    const [copied, setCopied] = useState(false);
-
-    const handleClick = async (e) => {
+  const handleClick = async (e) => {
     await copySnippet(e.currentTarget);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
@@ -13,20 +12,27 @@ function BotonCopiarCodigo({ copySnippet }) {
 
   return (
     <>
-    <button
-              type="button"
-             onClick={handleClick}
-              className="absolute right-2 top-2 p-2 rounded-md hover:bg-slate-700 transition-colors"
-              aria-label="Copiar código"
-            >
-              {copied ? (
-                <Check size={18} className="text-green-400" />
-              ) : (
-                <Copy size={18} className="text-slate-400" />
-              )}
-            </button>
+      <button
+        type="button"
+        onClick={handleClick}
+        className="absolute right-2 top-2 p-2 rounded-md hover:bg-slate-700 transition-colors color-black"
+        aria-label="Copiar código"
+        style={{
+          backgroundColor: "#272822",
+          padding: "12px 24px",
+          borderRadius: "8px",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        {copied ? (
+          <Check size={18} className="text-green-400" color="green" />
+        ) : (
+          <Copy size={18} className="text-slate-400" color="white" />
+        )}
+      </button>
     </>
-  )
+  );
 }
 
-export default BotonCopiarCodigo
+export default BotonCopiarCodigo;
