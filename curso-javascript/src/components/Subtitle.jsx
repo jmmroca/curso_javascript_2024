816f5aeb495e65db8164745bf95cf3a98ef134fe
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-16 14:18:05 +0100

    Change logo Javascript

--- curso-javascript/src/components/Subtitle.jsx
+++ curso-javascript/src/components/Subtitle.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 function Subtitle({ subtitulo }) {
+
+  // TODO: crear componente que ejecute el codigo de prueba
+  
+  
   return (
     <>
         <div id="subtitle-container">
           <div id="text">{subtitulo}</div>
-          <div id="logo-javascript">JS</div>
+          <div id="logo-javascript"><img src="./public/logo-javascript.png" alt="logo-javascript" /></div>
         </div>
     </>
   )
