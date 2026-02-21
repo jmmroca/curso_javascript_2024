923c93a13f16910885849f053c52b78956437184
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-21 22:34:34 +0100

    change button Barra Superior

--- curso-javascript/src/components/BarraSuperior.jsx
+++ curso-javascript/src/components/BarraSuperior.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
-import { dataCurso } from './data/dataCurso';
+import React from "react";
+import { useState } from "react";
+import { dataCurso } from "./data/dataCurso";
 
-function BarraSuperior({paginaActual, setPaginaActual}) {
-
+function BarraSuperior({ paginaActual, setPaginaActual }) {
   const [paginaMaxima, _setPaginaMaxima] = useState(dataCurso.paginas.length);
   return (
-    <div className='upperBar'>
-        <button onClick={() => setPaginaActual(paginaActual > 1 ? paginaActual - 1 : 1)}>Anterior</button>
-        <p>{paginaActual} de {paginaMaxima}</p>
-        <button onClick={() => setPaginaActual(paginaActual < paginaMaxima ? paginaActual + 1 : paginaMaxima)}>Siguiente</button>  
+    <div className="upperBar">
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => setPaginaActual(paginaActual > 1 ? paginaActual - 1 : 1)}
+      >
+        Anterior
+      </button>
+      <p>
+        {paginaActual} de {paginaMaxima}
+      </p>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() =>
+          setPaginaActual(
+            paginaActual < paginaMaxima ? paginaActual + 1 : paginaMaxima,
+          )
+        }
+      >
+        Siguiente
+      </button>
     </div>
-  )
+  );
 }
 
-export default BarraSuperior
+export default BarraSuperior;
--- curso-javascript/src/pages/CursoJavascript.jsx
+++ curso-javascript/src/pages/CursoJavascript.jsx
@@ -3,7 +3,6 @@ import MenuPrincipal from "../components/MenuPrincipal";
 import BarraSuperior from "../components/BarraSuperior";
 import MainContainer from "../components/MainContainer";
 import Variable from "../components/Variable";
-// import BotonMostarContainer from "../components/BotonMostarContainer";
 import Footer from "../components/Footer";
 import { useState } from "react";
 import LogoHeader from "../components/LogoHeader";
@@ -19,10 +18,6 @@ function CursoJavascript() {
           paginaActual={paginaActual}
           setPaginaActual={setPaginaActual}
         />
-        {/* <BotonMostarContainer
-          mostrarContainer={mostrarContainer}
-          setMostrarContainer={setMostrarContainer}
-        /> */}
         {mostrarContainer ? (
           <MainContainer paginaActual={paginaActual} />
         ) : (
