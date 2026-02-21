f9673fda1a6eaeda30828938c5b11163f7567d3e
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-21 21:15:49 +0100

    changed title.jsx

--- curso-javascript/src/components/Title.jsx
+++ curso-javascript/src/components/Title.jsx
@@ -1,3 +1,11 @@
+import codeCampusLogo from './assets/code_campus_logo.png'
+import concatenacionJs from './assets/concatenacion_javascript.png'
+import dataTypeJs from './assets/data_type_javascript.png'
+import logoJs from './assets/logo-javascript.png'
+import operadores from './assets/operadores.png'
+import variable from './assets/variable.jpg'
+import scopeJs from './assets/Scope-en-JavaScript.jpg'
+
 function Title( { titulo, imagen, imagenGrande } ) {
   return (
     <>
