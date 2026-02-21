96d1302ee9a387f82e345b44493f8e46ce61f950
Jose Manuel Martinez Roca <josemmroca@gmail.com>
2026-02-21 20:46:24 +0100

    updated config deploy

--- curso-javascript/src/main.jsx
+++ curso-javascript/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/curso_javascript_2024">
       <App />
     </BrowserRouter>
   </StrictMode>
